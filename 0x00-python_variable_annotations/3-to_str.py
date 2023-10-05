a465167959cfa12327a19a70dbaccd76ee435a53
vinsky001 <ekibet544@gmail.com>
2023-10-05 10:30:58 +0000

    type-annotated function concat that takes a string str1 and a string str2 as arguments and returns a concatenated string

---
+++ 0x00-python_variable_annotations/3-to_str.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+"""
+type-annotated function to_str that takes a float n as argument
+and returns the string representation of the float.
+"""
+
+
+def to_str(n: float) -> str:
+    """
+    takes a float n as argument
+    and returns the string representation of the float.
+    """
+    return str(n)
+
+    if __name__ == '__main__':
+        pi_str = to_str(3.14)
+        print(pi_str == str(3.14))
+        print(to_str.__annotations__)
+        print("to_str(3.14) returns {} whiich is a {}".format(pi_str, type(pi_str)))
