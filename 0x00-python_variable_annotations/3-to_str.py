2ec35a3b8501359bacbe9ad384c2ca40c6afe6ea
vinsky001 <ekibet544@gmail.com>
2023-10-06 08:52:29 +0000

    Fixed indentation error

--- 0x00-python_variable_annotations/3-to_str.py
+++ 0x00-python_variable_annotations/3-to_str.py
@@ -16,4 +16,5 @@ def to_str(n: float) -> str:
         pi_str = to_str(3.14)
         print(pi_str == str(3.14))
         print(to_str.__annotations__)
-        print("to_str(3.14) returns {} whiich is a {}".format(pi_str, type(pi_str)))
+        print("to_str(3.14) returns {} which is a {}".format(
+            pi_str, type(pi_str)))
