a2d17565272cd4d2cf71ecebdedb513153cd25dd
vinsky001 <ekibet544@gmail.com>
2023-10-07 09:15:56 +0000

    More involved type annotations

--- 0x00-python_variable_annotations/101-safely_get_value.py
+++ 0x00-python_variable_annotations/101-safely_get_value.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env python3
+i#!/usr/bin/env python3
 
 """
 Given the parameters and return values, add type annotations to the function
@@ -7,26 +7,25 @@ Hint: look into TypeVar
 """
 
 
-from typing import Union, Mapping, TypeVar, Any
+from typing import Dict, TypeVar, Optional
 
-T = TypeVar('T')
+
+# create a type variable for the key type
+K = Type("K")
 
 
 def safely_get_value(
-        dct: Mapping, key: Any, default: Union[T, None] = None
-        ) -> Union[None, T]:
-    """
-    Annotate function parameters and return values with the help of TypeVar
-    """
-    if key in dct:
+    dct: Dict[K, str],
+    key: K,
+    default: Optional[str] = None
+) -> Optional[str]:
+if key in dct:
         return dct[key]
     else:
         return default
 
-
-if __name__ == '__main__':
-    annotations = safely_get_value.__annotations__
-
-    print("Here's what the mappings should look like")
-    for k, v in annotations.items():
-        print(("{}: {}".format(k, v)))
+    if _name__ == '__main__':
+        annotations = safely_get_value.__annotations__
+        print("Here's what the mappings should look like")
+        for k, v in annotations.items():
+            print(("{}: {}".format(k, v)))
