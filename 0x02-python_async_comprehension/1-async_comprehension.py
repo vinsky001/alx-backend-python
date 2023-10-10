9f63b827a936891ffc86f5a2648952a8cded730e
vinsky001 <ekibet544@gmail.com>
2023-10-10 20:51:10 +0000

    Async Comprehensions

---
+++ 0x02-python_async_comprehension/1-async_comprehension.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python3
+"""
+1. Async Comprehensions
+"""
+
+import asyncio
+from typing import Generator, List
+
+async_generator = __import__('0-async_generator').async_generator
+
+
+async def async_comprehension() -> Generator[List[float], None, None]:
+    """
+    The coroutine will collect 10 random numbers using an async comprehensing
+    over async_generator, then return the 10 random numbers.
+    """
+    result = [i async for i in async_generator()]
+    return result  # type: ignore
+
+if __name__ == '__main__':
+    async def main():
+        print(await async_comprehension())
+
+    asyncio.run(main())
